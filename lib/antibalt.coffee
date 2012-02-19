5226679d3209051b9cfb057d2a328b5fc22e81aa
Paul Annesley <paul@annesley.cc>
2012-02-19 22:12:59 +1100

    Single escapee drawn once to screen as blue rectangle.

--- lib/antibalt.coffee
+++ lib/antibalt.coffee
@@ -3,3 +3,22 @@ canvas = document.getElementById("antibalt")
 canvas.width = 800
 canvas.height = 600
 canvas.style.backgroundColor = "black"
+
+context = canvas.getContext("2d")
+
+rgb = (r, g, b) -> (new Color(r, g, b)).toString()
+
+class Color
+  constructor: (@r, @g, @b) ->
+  toString: -> "rgb(#{@r},#{@g},#{@b})"
+
+class Escapee
+  WIDTH = 8
+  HEIGHT = 16
+  constructor: (@x, @y) ->
+    @color = rgb(64, 64, 255)
+  render: (context) ->
+    context.fillStyle = @color
+    context.fillRect(@x, @y, WIDTH, HEIGHT)
+
+new Escapee(100, 100).render(context)
--- public/js/antibalt.js
+++ public/js/antibalt.js
@@ -1,5 +1,5 @@
 (function() {
-  var canvas;
+  var Color, Escapee, canvas, context, rgb;
 
   canvas = document.getElementById("antibalt");
 
@@ -8,5 +8,51 @@
   canvas.height = 600;
 
   canvas.style.backgroundColor = "black";
+
+  context = canvas.getContext("2d");
+
+  rgb = function(r, g, b) {
+    return (new Color(r, g, b)).toString();
+  };
+
+  Color = (function() {
+
+    function Color(r, g, b) {
+      this.r = r;
+      this.g = g;
+      this.b = b;
+    }
+
+    Color.prototype.toString = function() {
+      return "rgb(" + this.r + "," + this.g + "," + this.b + ")";
+    };
+
+    return Color;
+
+  })();
+
+  Escapee = (function() {
+    var HEIGHT, WIDTH;
+
+    WIDTH = 8;
+
+    HEIGHT = 16;
+
+    function Escapee(x, y) {
+      this.x = x;
+      this.y = y;
+      this.color = rgb(64, 64, 255);
+    }
+
+    Escapee.prototype.render = function(context) {
+      context.fillStyle = this.color;
+      return context.fillRect(this.x, this.y, WIDTH, HEIGHT);
+    };
+
+    return Escapee;
+
+  })();
+
+  new Escapee(100, 100).render(context);
 
 }).call(this);
