de1f04a5d660f1c8fdaeb8ee657ce23f5ebbf03d
Paul Annesley <paul@annesley.cc>
2012-02-19 21:48:55 +1100

    Empty 800x600 canvas with black background.

---
+++ lib/antibalt.coffee
@@ -0,0 +1,5 @@
+canvas = document.getElementById("antibalt")
+
+canvas.width = 800
+canvas.height = 600
+canvas.style.backgroundColor = "black"
---
+++ public/js/antibalt.js
@@ -0,0 +1,12 @@
+(function() {
+  var canvas;
+
+  canvas = document.getElementById("antibalt");
+
+  canvas.width = 800;
+
+  canvas.height = 600;
+
+  canvas.style.backgroundColor = "black";
+
+}).call(this);
